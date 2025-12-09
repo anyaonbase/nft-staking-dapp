9fae5e227d3bbcdc4256461fb7f2ef43f2d60814
anyaonbase <anyaonbase@gmail.com>
2025-12-09 22:53:05 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { ThirdwebProvider, ConnectWallet, useAddress, useContract, Web3Button } from "thirdweb/react";
+import React, { useState } from "react";
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  useAddress,
+  useContract,
+  Web3Button,
+  useOwnedNFTs,
+  useNFTCollection,
+} from "thirdweb/react";
 
 const nftContractAddress = "0x09d21cc7a65e5efac638b31a185c00834f2e66bf";
 const stakingContractAddress = "0xBb580453030333aDA869A05b3aAFF048753F88A0";
-const rewardTokenAddress = "0x986Ffb3BFA039F3798B4e01a36C21CD721eD361c";
 
 function App() {
   const address = useAddress();
   const { contract: stakingContract } = useContract(stakingContractAddress);
+  const nftCollection = useNFTCollection(nftContractAddress);
+  const { data: ownedNFTs } = useOwnedNFTs(nftCollection, address);
+  const [selectedTokenId, setSelectedTokenId] = useState(null);
 
   return (
     <ThirdwebProvider desiredChainId={56}>
@@ -20,24 +30,49 @@ function App() {
           <>
             <p>Connected as: {address}</p>
 
-            <Web3Button
-              contractAddress={stakingContractAddress}
-              action={async (contract) => {
-                await contract.call("stake", [nftContractAddress]);
-              }}
-            >
-              Stake NFT
-            </Web3Button>
+            {ownedNFTs && ownedNFTs.length > 0 ? (
+              <div style={{ margin: "20px 0" }}>
+                <h3>Select an NFT to Stake/Unstake:</h3>
+                <select
+                  value={selectedTokenId || ""}
+                  onChange={(e) => setSelectedTokenId(e.target.value)}
+                >
+                  <option value="" disabled>
+                    -- Select NFT --
+                  </option>
+                  {ownedNFTs.map((nft) => (
+                    <option key={nft.metadata.id} value={nft.metadata.id}>
+                      #{nft.metadata.id} {nft.metadata.name || ""}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            ) : (
+              <p>You don't own any NFTs from this collection.</p>
+            )}
 
-            <Web3Button
-              contractAddress={stakingContractAddress}
-              action={async (contract) => {
-                await contract.call("unstake", [nftContractAddress]);
-              }}
-              style={{ marginLeft: "10px" }}
-            >
-              Unstake NFT
-            </Web3Button>
+            <div style={{ marginTop: "20px" }}>
+              <Web3Button
+                contractAddress={stakingContractAddress}
+                action={async (contract) => {
+                  if (!selectedTokenId) return alert("Please select an NFT");
+                  await contract.call("stake", [selectedTokenId]);
+                }}
+              >
+                Stake NFT
+              </Web3Button>
+
+              <Web3Button
+                contractAddress={stakingContractAddress}
+                action={async (contract) => {
+                  if (!selectedTokenId) return alert("Please select an NFT");
+                  await contract.call("unstake", [selectedTokenId]);
+                }}
+                style={{ marginLeft: "10px" }}
+              >
+                Unstake NFT
+              </Web3Button>
+            </div>
           </>
         )}
       </div>
